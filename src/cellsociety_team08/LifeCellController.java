e565b9c17555e1237d64b5df7d08ef02e33595a3
Jeffrey <jql3@duke.edu>
2018-02-02 21:50:04 -0500

    moved constructor into LifeController class, moved some methods around, implemented colors

--- src/cellsociety_team08/LifeCellController.java
+++ src/cellsociety_team08/LifeCellController.java
@@ -1,15 +1,34 @@
 package cellsociety_team08;
 
+import java.awt.Color;
+
 public class LifeCellController extends CellController {
 	public LifeCellController(int[] dimensions, int[][] cellsOn) {
 		super(dimensions, cellsOn);
+		for (int x = 0; x < xSize; x++) {
+			for (int y = 0; y < ySize; y++) {
+			Cell tempCell = new	Cell("off");
+			cellGrid[x][y] = tempCell;	
+			tempCell.setState(Color.WHITE);
+			}
+		}
+		
+		for (int z = 0; z < cellsOn.length; z++) {			
+			int xCoord = cellsOn[z][0];
+			int yCoord = cellsOn[z][1];
+			cellGrid[xCoord][yCoord] = new Cell("on");
+			cellGrid[xCoord][yCoord].setState(Color.BLACK);
+			
+		}
+		initializeNeighbors();
 	}
 
 	// for each cell, check how many neighbors are on
 	public void setNextStates() {
-		for (int x = 0; x < cellGrid.length; x++) {
-			for (int y = 0; y < cellGrid[x].length; y++) {
-				String[] stateNameList = cellGrid[x][y].getNeighborStateNames();
+		for (int x = 0; x < xSize; x++) {
+			for (int y = 0; y < ySize; y++) {
+				Cell toSet = retrieveCell(x,y);
+				String[] stateNameList = toSet.getNeighborStateNames();
 				int numberOn = 0;
 	
 				for (int z = 0; z < stateNameList.length; z++) {
@@ -21,48 +40,43 @@ public class LifeCellController extends CellController {
 					}
 				}
 
-						if (cellGrid[x][y].getState().getStateName().equals("on")) {
+						if (toSet.getState().equals("on")) {
 							if (numberOn < 2 || numberOn > 3) {
-								State temp = new State("off");
-								cellGrid[x][y].nextState = temp; // if cell is on and has either <2 or >3
+								toSet.nextState = "off"; // if cell is on and has either <2 or >3
 																				// neighbors, then turn next state off
 							}
 							else {
-								State temp = new State("on");    // otherwise keep next state on
-								cellGrid[x][y].nextState = temp;
+								
+								toSet.nextState = "on";
 							}
 						} else {
 							if (numberOn == 3) {
-								State temp = new State("on");
-								cellGrid[x][y].nextState = temp; // if cell is off, and 3 cells around are on																	// on,
+								toSet.nextState = "on"; // if cell is off, and 3 cells around are on																	// on,
 																			// then turn next state on
 							}
 							else {
-								State temp = new State("off");    // otherwise keep next state off
-								cellGrid[x][y].nextState = temp;
+								    // otherwise keep next state off
+								toSet.nextState = "off";
 							}
 						}
+						if (toSet.nextState.equals("on")) {
+							toSet.setState(Color.BLACK);
+						}
+						else {
+							toSet.setState((Color.WHITE));
+						}
+						
 					
 				}
 			}
 		}
 	
 
-	public void updateCells() {
-		for (int x = 0; x < cellGrid.length; x++) {
-			for (int y = 0; y < cellGrid[x].length; y++) {
-				cellGrid[x][y].updateState();
-				
-			}
-		}
-	}
-
 	public void printCells() {
 		for (int x = 0; x < cellGrid.length; x++) {
 			System.out.println("");
 			for (int y = 0; y < cellGrid[x].length; y++) {
-//				System.out.print(x + " " + y);
-				System.out.print(cellGrid[x][y].getState().getStateName() + " ");
+				System.out.print(cellGrid[x][y].getState() + " " );
 			}
 		}
 		System.out.println("");
