f6ae43364643dad3d11c273842bee3e909d7e509
Julia Long <julia.long@duke.edu>
2018-02-08 17:26:46 -0500

    Added addpoint method to graph class

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,10 @@
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Graph {
 
     public final int GRAPH_X_SIZE = 600;
@@ -9,16 +13,25 @@ public class Graph {
     private Rectangle myGraph;
     private Color[] plotColors;
 
-    private int[] currentX;
-    private int[] currentY;
+    private Map<String, ArrayList<Double>> xPositions;
+    private Map<String, ArrayList<Double>> yPositions;
 
     public Graph(int numTypes) {
         myGraph = new Rectangle(GRAPH_X_SIZE, GRAPH_Y_SIZE);
         plotColors = new Color[numTypes];
+        xPositions = new HashMap<String, ArrayList<Double>>();
+        yPositions = new HashMap<String, ArrayList<Double>>();
     }
 
-    public void addPoint() {
-
+    public void addPoint(String cellType, Double x, Double y) {
+        if (!xPositions.keySet().contains(cellType)) {
+            xPositions.put(cellType, new ArrayList<>());
+            yPositions.put(cellType, new ArrayList<>());
+        }
+        else {
+            xPositions.get(cellType).add(x);
+            yPositions.get(cellType).add(y);
+        }
     }
 
 }
